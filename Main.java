fcf6a17fea751ec2097cf586fdb48bdaeee83a13
AbijitE <eabijit746@gmail.com>
2023-11-10 12:02:00 +0530

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+package com.java;
+
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("This is my first project to commit in Git Repository");
+	System.out.println(new Date());
+	System.out.println("Updated");
+	}
+
+}
